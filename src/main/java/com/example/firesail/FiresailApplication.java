725f1ec39bcebdf9b12339d2d673a3cc062a045e
Ravinder Chauhan <ravchauhan@paypal.com>
2022-02-02 22:07:15 +0530

    Base Setup Using Spring Initializer With No Extra Dependency

---
+++ src/main/java/com/example/firesail/FiresailApplication.java
@@ -0,0 +1,13 @@
+package com.example.firesail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiresailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiresailApplication.class, args);
+	}
+
+}
